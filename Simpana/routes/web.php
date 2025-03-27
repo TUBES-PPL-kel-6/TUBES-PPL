32fd6d3c794ed992511a0433028029464985c8f2
K1ro16 <freedomkiro16@gmail.com>
2025-03-27 22:20:05 +0700

    tes SMP-52
    SMP-52 tes

--- Simpana/routes/web.php
+++ Simpana/routes/web.php
@@ -10,4 +10,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/', function () {
+    return view('welcome');
+});
 
