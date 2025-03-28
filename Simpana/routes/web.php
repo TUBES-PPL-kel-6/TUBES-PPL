f5999f8090feeb249eb09b568d866d993153741d
K1ro16 <freedomkiro16@gmail.com>
2025-03-28 22:22:20 +0700

    SMP-26
    Bikin UI pembayaran

--- Simpana/routes/web.php
+++ Simpana/routes/web.php
@@ -5,9 +5,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('landingPage');
-
 });
 
 Route::get('/login', function () {
     return view('login');
+});
+
+Route::get('/payment', function () {
+    return view('payment');
 });
