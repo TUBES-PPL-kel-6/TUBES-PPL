0e707ed43d5c6d5899cd9617d3c4490d5d531b5f
Anon9m0us <freedomkiro16@gmail.com>
2025-05-04 16:41:12 +0700

    SMP-30
    Membuat simpanan wajib dan sukarela

--- Simpana/routes/web.php
+++ Simpana/routes/web.php
@@ -14,6 +14,8 @@
  use App\Http\Controllers\DiscussionController;
  use App\Http\Controllers\PaymentController;
  use App\Http\Controllers\ProfileController;
+ use App\Http\Controllers\SimpananController;
+ 
  
  // Public routes
  Route::get('/', function () {
@@ -116,3 +118,10 @@ Route::get('/notifications/pinjaman', function () {
 route::get ('/general', function () {
     return view('payment-form');
 })->name('payment-form');
+
+// Add these routes for the simpanan functionality
+Route::prefix('dashboard')->name('dashboard.')->middleware(['auth'])->group(function() {
+    Route::get('/simpanan', [SimpananController::class, 'index'])->name('simpanan');
+    Route::get('/simpanan/create', [SimpananController::class, 'create'])->name('simpanan.create');
+    Route::post('/simpanan', [SimpananController::class, 'store'])->name('simpanan.store');
+});
